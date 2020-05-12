b7959a581c53084baca62c4d60ab3e5c8f197655
binxu <binxu.wang@wustl.edu>
2020-05-12 03:31:52 -0500

    adding cluster script

--- cluster_scripts/prepare_GAN_imgpair.sh
+++ cluster_scripts/prepare_GAN_imgpair.sh
@@ -2,7 +2,7 @@
 #PBS -N prepare_GAN_imagepair
 #PBS -m be
 #PBS -q dque
-#PBS -t 1-7
+#PBS -t 1-8
 #PBS -l nodes=1:ppn=1:gpus=1,walltime=10:00:00,mem=15gb
 
 # Prepare the virtual env for python
@@ -13,7 +13,8 @@ param_list='1 1000
 3001 4000
 4001 5000
 5001 6000
-6001 7000'
+6001 7000
+7001 8000'
 export csr_lim="$(echo "$param_list" | head -n $PBS_ARRAYID | tail -1)"
 
 cd ~/Visual_Neuro_InSilico_Exp/
