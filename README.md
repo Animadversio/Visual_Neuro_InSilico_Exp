7df94ff65a1d552e408a946ec27a62bcabdb060f
Binxu Wang <lio50328@pku.edu.cn>
2020-06-23 23:14:57 -0500

    Add code on sim hession

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This project serves the purpose of developping and testing computational tools *
     - The older one in `Optimizer` CMA-ES, Cholesky CMA-ES, GA, CholeskyCMAES_Sphere
     - The series of optimizer based on the Hessian Aware ZO Optimization paper `ZO_HessAware_Optimizers`
     - The optimizers based on Powell's method `PowellOptimizers`
-* Some objects target at estimating and analyzing Hessian for Black Box functions. 
+* Some objects target at estimating and analyzing Hessian for Black Box and non linear functions. 
     - `pytorch_CNN_hessian` 
     - `pytorch_GAN_similarity_hessian` 
     - A collection of functions to help analyze hessian matrix and spectrum `hessian_analysis`
@@ -19,4 +19,4 @@ This project serves the purpose of developping and testing computational tools *
     - A non-gradient Hessian estimation method inspired by the HessAware ZOO paper `ZO_Hessian_Estim`
 
 ## Usage 
-This Repo can potentially be deployed onto cluster to run at large scale, based on Caffe or TF backend, and deploy using Torch backend.
+This Repo can potentially be deployed onto cluster to run at large scale, based on Caffe or TF or torch backend.
---
+++ pytorch_GAN_sim_hessian_pure.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from GAN_utils import upconvGAN
+from hessian import hessian
+sys.path.append(r"D:\Github\PerceptualSimilarity")
+sys.path.append(r"E:\Github_Projects\PerceptualSimilarity")
+import models  # from PerceptualSimilarity folder
+model_vgg = models.PerceptualLoss(model='net-lin', net='vgg', use_gpu=1, gpu_ids=[0])
+
+G = upconvGAN("fc6")
+def sim_hessian_computation(z, percept_loss, savepath=None):
+    """
+    Depending on Generator imported from caffe to pytorch.
+    Depending on Hessian, and autograd
+
+    :param z: vector to compute hessian at
+    :param percept_loss: the model from PerceptualSimilarity package
+    :return: H: Hessian Matrix
+     eigval: eigen decomposition, eigen values
+     eigvec: eigen vectors
+     gradient: gradient from the spot
+     d_sim: similarity metric
+    """
+    feat = torch.from_numpy(z).requires_grad_(False)
+    img_ref = G.visualize(feat,)  # forward the feature vector through the GAN
+    # Perturbation vector for gradient
+    perturb_vec = torch.zeros((1, 4096), dtype=float32).requires_grad_(True)
+    perturb_vec = 0.00001*torch.randn((1, 4096), dtype=float32).requires_grad_(True)
+    img_pertb = G.visualize(feat + perturb_vec)
+    d_sim = percept_loss.forward(img_ref, img_pertb)
+    gradient = torch.autograd.grad(d_sim, perturb_vec, retain_graph=True)[0]
+    H = hessian(d_sim[0, 0, 0, 0], perturb_vec, create_graph=False)  # 10min for computing a Hessian
+    eigval, eigvec = np.linalg.eigh(H.cpu().detach().numpy())  # eigen decomposition for a symmetric array! ~ 5.7 s
+    # Print statistics
+    pos1_nums = (eigval > 1).sum()
+    pos_nums = (eigval > 0.1).sum()
+    pos01_nums = (eigval > 0.01).sum()
+    num01 = (np.logical_and(eigval < 0.1, eigval > -0.1)).sum()
+    num001 = (np.logical_and(eigval < 0.01, eigval > -0.01)).sum()
+    num0001 = (np.logical_and(eigval < 0.001, eigval > -0.001)).sum()
+    neg1_nums = (eigval < - 1).sum()
+    neg_nums = (eigval < - 0.1).sum()
+    print("[1, inf]:%d; [0.1, inf]:%d; [0.01, inf]:%d, [-0.1,0.1]: %d; [-0.01,0.01]: %d; [-0.001,0.001]: %d; [-inf, -0.1]:%d; [-inf, -1]:%d" % (
+            pos1_nums, pos_nums, pos01_nums, num01, num001, num0001, neg_nums, neg1_nums))
+    # Save to disk
+    if savepath is not None:
+        np.savez(savepath,
+             z=feat.detach().numpy(),
+             activation=d_sim.cpu().detach().numpy(),
+             grad=gradient.cpu().numpy(), H=H.cpu().detach().numpy(),
+             heig=eigval, heigvec=eigvec)
+        # join(output_dir, "hessian_sim_alex_lin_%s_%d.npz" % (unit[1], unit[2]))
+    return H, eigval, eigvec, gradient, d_sim
+
+z = torch.randn(1, 4096) * 3
+H, eigval, eigvec, gradient, d_sim = sim_hessian_computation(z, model_vgg)
