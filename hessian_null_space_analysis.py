d8c5b556416472a6a97df1ce487e5f1286732988
Animadversio <binxu.wang@wustl.edu>
2020-07-08 22:31:45 -0500

    Add Hessian Null space analysis

--- hessian_null_space_analysis.py
+++ hessian_null_space_analysis.py
@@ -7,6 +7,7 @@ from lanczos_generalized import lanczos_generalized
 from GAN_hvp_operator import GANHVPOperator, GANForwardHVPOperator, compute_hessian_eigenthings
 #
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pylab as plt
 from time import time
 from os.path import join
@@ -50,21 +51,201 @@ for imgi, code in enumerate(pasu_codes[:, :]):
 imgi, imgj = 0, 1
 with np.load(join(out_dir, "pasu_%03d.npz" % imgi)) as data:
     basisi = data["eigvects"]
+    eigvi = data["eigvals"]
+    codei = data["code"]
 
 with np.load(join(out_dir, "pasu_%03d.npz" % imgj)) as data:
     basisj = data["eigvects"]
+    eigvj = data["eigvals"]
+    codej = data["code"]
+
 #%%
 from sklearn.cross_decomposition import CCA
-def cca_correlation(X, Y, n_comp=50):
+def cca_subspace(X, Y, n_comp=50, **kwargs):
     """
-    :param X, Y: should be N-by-p, N-by-q matrices,
+    :param X, Y: should be N-by-p, N-by-q matrices, N is the dimension for the whole space, p, q are number of basis
+                 vectors (Note p, q functions as number of features to be recombined, while N functions as number of
+                 sampled). CCA will maximize
     :param n_comp: a integer, how many components we want to create and compare.
     :return: cca_corr, n_comp-by-n_comp matrix
        X_c, Y_c will be the linear mapped version of X, Y with shape  N-by-n_comp, N-by-n_comp shape
        cc_mat is the
     """
-    cca = CCA(n_components=n_comp)
+    cca = CCA(n_components=n_comp, **kwargs)
     X_c, Y_c = cca.fit_transform(X, Y)
     ccmat = np.corrcoef(X_c, Y_c, rowvar=False)
     cca_corr = np.diag(ccmat[n_comp:, :n_comp])  # slice out the cross corr part
-    return cca_corr
+    return cca_corr, cca
+
+#%%
+%%time
+t0 = time()
+cca_corr200, cca200 = cca_subspace(basisi[-200:, :].T, basisj[-200:, :].T, n_comp=200, max_iter=1000)
+print(time() - t0) # 57.68 sec / 86.67
+t0 = time()
+cca_corr100, cca100 = cca_subspace(basisi[-100:, :].T, basisj[-100:, :].T, n_comp=100, max_iter=1000)
+print(time() - t0) # 11.68 sec
+t0 = time()
+cca_corr50, cca50 = cca_subspace(basisi[-50:, :].T, basisj[-50:, :].T, n_comp=50)
+print(time() - t0) # 2 sec
+#%%
+t0 = time()
+cca_corr400, cca400 = cca_subspace(basisi[-400:, :].T, basisj[-400:, :].T, n_comp=400, max_iter=1000)
+print(time() - t0) #
+t0 = time()
+cca_corr400_ctr, cca400_ctr = cca_subspace(np.random.randn(400, 4096).T, np.random.randn(400, 4096).T, n_comp=400, max_iter=1000)
+print(time() - t0)
+# 98s
+#%%
+t0 = time()
+cca_corr200_ctr, cca200_ctr = cca_subspace(np.random.randn(200, 4096).T, np.random.randn(200, 4096).T, n_comp=200, max_iter=1000)
+cca_corr100_ctr, cca100_ctr = cca_subspace(np.random.randn(100, 4096).T, np.random.randn(100, 4096).T, n_comp=100, max_iter=1000)
+cca_corr50_ctr, cca50_ctr = cca_subspace(np.random.randn(50, 4096).T, np.random.randn(50, 4096).T, n_comp=50)
+print(time() - t0) # 2 sec
+# 509 s
+#%%
+plt.figure()
+plt.plot(cca_corr400, label="top400 eig")
+plt.plot(cca_corr200, label="top200 eig")
+plt.plot(cca_corr100, label="top100 eig")
+plt.plot(cca_corr50, label="top50 eig")
+plt.plot(cca_corr400_ctr, label="random 400")
+plt.plot(cca_corr200_ctr, label="random 200")
+plt.plot(cca_corr100_ctr, label="random 100")
+plt.plot(cca_corr50_ctr, label="random 50")
+plt.title("Top Eigen Space Is Shared\nMeasured by CCA")
+plt.ylabel("Correlation Value")
+plt.xlabel("CC #")
+plt.legend()
+plt.savefig(join(out_dir, "Pasu12_Shared_EigenSpace.jpg"))
+plt.show()
+#%%
+from PIL import Image
+def visualize_np(G, code, layout=None, show=True):
+    with torch.no_grad():
+        imgs = G.visualize(torch.from_numpy(code).float().cuda()).cpu().permute([2, 3, 1, 0]).squeeze().numpy()
+    if show:
+        if len(imgs.shape) <4:
+            plt.imshow(imgs)
+            plt.show()
+        else:
+            img_list = [imgs[:,:,:,imgi].squeeze() for imgi in range(imgs.shape[3])]
+            if layout is None:
+                mtg = build_montages(img_list,(256,256),(imgs.shape[3],1))[0]
+                Image.fromarray(np.uint8(mtg*255.0)).show()
+            else:
+                mtg = build_montages(img_list, (256, 256), layout)[0]
+                Image.fromarray(np.uint8(mtg*255.0)).show()
+    return imgs
+#%%
+cutoff = 800
+visualize_np(G, pasu_codes[:20,:]@basisi[:,:].T@basisi[:,:], (4,5))
+#%%
+imgn = pasu_codes.shape[0]
+basis_col = []
+eigv_col = []
+for imgi in range(imgn):
+    with np.load(join(out_dir, "pasu_%03d.npz" % imgi)) as data:
+        basisi = data["eigvects"]
+        eigvi = data["eigvals"]
+        basis_col.append(basisi)
+        eigv_col.append(eigvi)
+#%% Averaged Hessian
+avg_Hess = np.zeros((4096, 4096))
+for imgi in range(imgn):
+    basisi = basis_col[imgi]
+    eigvi = eigv_col[imgi]
+    avg_Hess = avg_Hess + (basisi.T * eigvi[np.newaxis, :] @ basisi)
+
+avg_Hess = avg_Hess / imgn
+#%%
+eigv_avg, eigvect_avg = np.linalg.eigh(avg_Hess)
+#%%
+np.savez(join(out_dir, "Pasu_Space_Avg_Hess.npz"), H_avg=avg_Hess, eigv_avg=eigv_avg, eigvect_avg=eigvect_avg)
+#%%
+def SLERP(code1, code2, steps, lim=(0,1)):
+    """Spherical Linear Interpolation"""
+    code1, code2 = code1.squeeze(), code2.squeeze()
+    cosang = np.dot(code1, code2) / norm(code1) / norm(code2)
+    angle = np.arccos(cosang)
+    ticks = angle * np.linspace(lim[0], lim[1], steps, endpoint=True)
+    slerp_code = np.sin(angle - ticks)[:,np.newaxis] * code1[np.newaxis, :] + np.sin(ticks)[:,np.newaxis] * \
+                 code2[np.newaxis, :]
+    return slerp_code
+
+def LERP(code1, code2, steps, lim=(0,1)):
+    """Spherical Linear Interpolation"""
+    code1, code2 = code1.reshape(1,-1), code2.reshape(1,-1)
+    ticks = np.linspace(lim[0], lim[1], steps, endpoint=True)[:,np.newaxis]
+    slerp_code = (1 - ticks) @ code1 + ticks @ code2
+    return slerp_code
+#%%
+proj_rang = range(2000,3500)
+proj_op = eigvect_avg[proj_rang,:].T @ eigvect_avg[proj_rang,:]
+visualize_np(G, SLERP(pasu_codes[0:1,:]@proj_op, pasu_codes[10:11,:]@proj_op, 11),)
+#%%
+proj_coef = pasu_codes @ basisi.T
+proj_coef[:, :400] = np.mean(proj_coef[:, :400], axis=0)
+recon_code = proj_coef @ basisi
+visualize_np(G, recon_code, (12, 16))
+#%%
+visualize_np(G, pasu_codes @ basis_col[20].T @ basis_col[20], (12, 16))
+#%%
+visualize_np(G, pasu_codes @ basis_col[20].T @ basis_col[20], (12, 16))
+#%%
+visualize_np(G, pasu_codes @ eigvect_avg[:,-100:] @ eigvect_avg[:,-100:].T, (12, 16))
+#%%
+proj_coef = pasu_codes @ eigvect_avg
+proj_coef[:, :-150] = np.mean(proj_coef[:, :-150], axis=0)
+recon_code = proj_coef @ eigvect_avg.T
+visualize_np(G, recon_code, (12, 16))
+#%%
+pasu_mean = np.mean(pasu_codes,axis=0,keepdims=True)
+visualize_np(G, pasu_mean+(pasu_codes - pasu_mean)@ eigvect_avg[:,-200:] @ eigvect_avg[:,-200:].T, (12, 16))
+#%%
+pasu_codes_rd = pasu_mean+(pasu_codes - pasu_mean)@ eigvect_avg[:,-200:] @ eigvect_avg[:,-200:].T
+from sklearn.decomposition.pca import PCA
+code_PCA = PCA().fit(pasu_codes_rd)
+#%%
+visualize_np(G, LERP(pasu_codes_rd[0:1,:], pasu_codes_rd[50:51,:], 11),)
+#%%
+evo_code_path = r"N:\Stimuli\2019-12-Evolutions\2019-12-30-Beto-03\2019-12-30-12-30-57\block032_thread000_code.mat"
+data = loadmat(evo_code_path)
+evo_codes = data['codes']
+#%%
+# visualize_np(G, evo_codes @ basisi.T @ basisi, (6, 7))
+visualize_np(G, evo_codes @ eigvect_avg[:,-800:] @ eigvect_avg[:,-800:].T, (6, 7))
+#%%
+evo_mean = evo_codes.mean(axis=0)
+evo_codes_rd = evo_mean + (evo_codes - evo_mean) @ eigvect_avg[:,-200:] @ eigvect_avg[:,-200:].T
+visualize_np(G, evo_codes_rd, (7, 6))
+#%%
+visualize_np(G, evo_codes, (7, 6))
+#%%
+from os import listdir
+from os.path import isdir
+from glob import glob
+out_dir = r"E:\OneDrive - Washington University in St. Louis\HessTune\NullSpace"
+evol_dir = r"N:\Stimuli\2019-12-Evolutions"
+expnames = sorted(listdir(evol_dir))
+expnames = [expname for expname in expnames if ("Alfa" in expname) or ("Beto" in expname)]
+expname = expnames[1]
+for expi, expname in enumerate(expnames):
+    subname = [fn for fn in listdir(join(evol_dir, expname)) if isdir(join(evol_dir, expname, fn))]
+    if len(subname) > 0:
+        matlist = sorted(glob(join(evol_dir, expname, subname[0], "*.mat")))
+        evo_code_path = matlist[-2]
+        data = loadmat(evo_code_path)
+        evo_codes = data['codes']
+        code = evo_codes[0:1, :]
+        nameparts = evo_code_path.split("\\")
+        t0 = time()
+        feat = torch.from_numpy(code)
+        feat.requires_grad_(False)
+        metricHVP = GANHVPOperator(G, feat, model_squ)
+        eigvals, eigvects = lanczos(metricHVP, num_eigenthings=800, use_gpu=True)
+        print("Finish computing expi %d %.2f sec passed, max %.2e min %.2e 10th %.1e 50th %.e 100th %.1e (norm %.1f)"
+          % (expi, time() - t0,max(np.abs(eigvals)), min(np.abs( eigvals)), eigvals[ -10], eigvals[-50],eigvals[-100],
+          norm(code)))
+        np.savez(join(out_dir, "evol_%03d.npz" % expi), eigvals=eigvals, eigvects=eigvects, code=code,
+                 source=evo_code_path)
