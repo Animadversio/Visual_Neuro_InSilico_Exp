77f9f3e35883b503712a84d30e503f7c9ea184d3
binxu <binxu.wang@wustl.edu>
2020-07-14 00:05:21 -0500

    Add analysis and visualization to see the effects of different eigen subspace.

--- cluster_scripts/insilico_ZOHARD_cluster.py
+++ cluster_scripts/insilico_ZOHARD_cluster.py
@@ -146,11 +146,11 @@ def max_score_fun(scores_all, generations):
     return lastgen_max
     # lastgen_max = [scores_all[generations == geni].max() for geni in
     #                range(generations.max() - 10, generations.max() + 1)]
-best_scores_col = np.zeros((6, 5, 33, 5))
+best_scores_col = np.zeros((6, 1, 33, 5))
 for triali in range(5):
     for ui, unit in enumerate([("alexnet", 'fc8'), ("alexnet", 'fc6'), ("alexnet", 'conv5'), ("alexnet", 'conv4'), ("alexnet", 'conv2'), ("alexnet", 'conv1')]):
         netname, layer = unit
-        for chi in range(5):
+        for chi in range(1):
             savedir = os.path.join(recorddir, "%s_%s_%d" % (netname, layer, chi))
             for si, subspace_d in enumerate([50, 100, 200, 400]):
                 for oi, ofs in enumerate([1, 100, 200, 500, 1000, 2000, 3000]):
@@ -169,11 +169,33 @@ for triali in range(5):
                 generations = data["generations"]
                 best_scores_col[ui, chi, -1, triali] = max_score_fun(scores_all, generations)
 #%%
+label_list = []
+for subspace_d in [50, 100, 200, 400]:
+    for ofs in [1, 100, 200, 500, 1000, 2000, 3000]:
+        label_list.append("%d-%d"%(ofs, ofs+subspace_d))
+    label_list.append("rand%d"%(subspace_d))
+label_list.append("full")
+
+#%%
+figdir = r"E:\Monkey_Data\Generator_DB_Windows\data\with_CNN"
 mean_score = best_scores_col.mean((1,3))
-plt.matshow(mean_score / mean_score[:,-1:])
-plt.ylim([-0.5,5.5])
-plt.yticks(range(6),["fc8","fc6","conv5","conv4","conv2","conv1"])
+fig = plt.figure(figsize=[13, 3.5])
+ax = fig.subplots()
+MAT = ax.matshow(mean_score / mean_score[:,-1:])
+for (i, j), z in np.ndenumerate(mean_score / mean_score[:,-1:]):
+    ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center',color='white')
+plt.ylim([-0.5, 5.5])
+plt.xlim([-0.5, 32.5])
+plt.yticks(range(6), ["fc8","fc6","conv5","conv4","conv2","conv1"])
+plt.ylabel("layers in AlexNet")
+plt.xlabel("Subspace in the fc6GAN")
+# plt.clabel("Activation relative to full space evolution")
+plt.xticks(ticks=range(33), labels=label_list, rotation=30)
+fig.colorbar(MAT)
+plt.tight_layout()
+plt.savefig(join(figdir, "eig_subsp_scores_heatmap.jpg"))
 plt.show()
+np.savez(join(figdir, "eig_subsp_max_scores.npz"), label_list=label_list, layers=["fc8","fc6","conv5","conv4","conv2","conv1"], best_scores=best_scores_col)
 #%%
 netname, layer = unit
 n_gen = 100
