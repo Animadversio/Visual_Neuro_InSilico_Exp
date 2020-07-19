6376631f7bb7db42d66b1453e3eac39ab7e06cf1
Animadversio <binxu.wang@wustl.edu>
2020-07-19 03:42:40 -0500

    Update StyleGAN2 Experiments

--- cluster_scripts/StyleGAN2_hessian.sh
+++ cluster_scripts/StyleGAN2_hessian.sh
@@ -6,7 +6,7 @@
 # Specify the default queue for the fastest nodes
 #PBS -m be
 #PBS -q dque
-#PBS -t 1-6
+#PBS -t 10-15
 
 # Prepare the virtual env for python
 # export PATH=/act/Anaconda3-2.3.0/bin:${PATH}
