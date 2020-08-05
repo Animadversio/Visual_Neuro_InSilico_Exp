026a27e94750c7e988bf31852f6fa1d691d96a9c
Animadversio <binxu.wang@wustl.edu>
2020-08-04 20:53:46 -0500

    Add experimental code of Basin CMA to cluster

--- BasinCMA.py
+++ BasinCMA.py
@@ -1,50 +1,78 @@
 import cma
 import tqdm
-from pytorch_pretrained_biggan import BigGAN, truncated_noise_sample
+from pytorch_pretrained_biggan import BigGAN, truncated_noise_sample, BigGANConfig
 import torch
 import torchvision
 from torchvision.transforms import ToPILImage
 from torchvision.utils import make_grid
 from torch.optim import SGD, Adam
+import os
+import sys
 import numpy as np
+import matplotlib.pylab as plt
 import pandas as pd
 from os.path import join
 from imageio import imread
 from scipy.linalg import block_diag
 #%%
-import sys
-sys.path.append(r"D:\Github\PerceptualSimilarity")
-sys.path.append(r"E:\Github_Projects\PerceptualSimilarity")
+def get_BigGAN(version="biggan-deep-256"):
+    cache_path = "/scratch/binxu/torch/"
+    cfg = BigGANConfig.from_json_file(join(cache_path, "%s-config.json" % version))
+    BGAN = BigGAN(cfg)
+    BGAN.load_state_dict(torch.load(join(cache_path, "%s-pytorch_model.bin" % version)))
+    return BGAN
+
+if sys.platform == "linux":
+    BGAN = get_BigGAN()
+    sys.path.append(r"/home/binxu/PerceptualSimilarity")
+    Hpath = r"/scratch/binxu/GAN_hessian/BigGAN/summary/H_avg_1000cls.npz"
+    imgfolder = r"/scratch/binxu/Datasets/ImageTranslation/GAN_real/B/train"
+    savedir = r"/scratch/binxu/GAN_invert/BasinCMA/ImageNet"
+else:
+    BGAN = BigGAN.from_pretrained("biggan-deep-256")
+    sys.path.append(r"D:\Github\PerceptualSimilarity")
+    sys.path.append(r"E:\Github_Projects\PerceptualSimilarity")
+    Hpath = r"E:\OneDrive - Washington University in St. Louis\Hessian_summary\BigGAN\H_avg_1000cls.npz"
+    imgfolder = r"E:\Cluster_Backup\Datasets\ImageTranslation\GAN_real\B\train"
+    # savedir = r"E:\Cluster_Backup\BigGAN_invert\ImageNet"
+    savedir = r"E:\OneDrive - Washington University in St. Louis\BigGAN_invert\BasinCMA\ImageNet"
+#%%
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("--ImDist", type=str, default="squeeze", help="Network model to use for Image distance computation")
+parser.add_argument("--dataset", type=str, default="ImageNet", help="")
+parser.add_argument("--imgidx", type=int, nargs='+', default=[200, 220], help="Network model to use for Image distance "
+                                                                   "computation")
+parser.add_argument("--cmasteps", type=int, default=10, help="")
+parser.add_argument("--gradsteps", type=int, default=10, help="")
+parser.add_argument("--finalgradsteps", type=int, default=500, help="")
+parser.add_argument("--CMApostGrad", type=bool, default=True, help="")
+parser.add_argument("--basis", type=str, default="all", help="")
+args = parser.parse_args([])
+#%%
 import models  # from PerceptualSimilarity folder
-ImDist = models.PerceptualLoss(model='net-lin', net='squeeze', use_gpu=1, gpu_ids=[0])
+ImDist = models.PerceptualLoss(model='net-lin', net=args.ImDist, use_gpu=1, gpu_ids=[0])
 for param in ImDist.parameters():
     param.requires_grad_(False)
 def L1loss(target, img):
     return (img - target).abs().sum(axis=1).mean(axis=1)
 
-alpha = 5 # relative weight
-#%%
-BGAN = BigGAN.from_pretrained("biggan-deep-256")
+# alpha = 5 # relative weight
 BGAN.cuda()
 BGAN.eval()
 for param in BGAN.parameters():
     param.requires_grad_(False)
-#%%
-savedir = r"E:\OneDrive - Washington University in St. Louis\BigGAN_invert\BasinCMA"
-data = np.load(r"E:\OneDrive - Washington University in St. Louis\Hessian_summary\BigGAN\H_avg_1000cls.npz")
+#%% Load the precomputed Hessian and Form them as basis matrix
+data = np.load(Hpath)
 evc_clas = torch.from_numpy(data['eigvects_clas_avg'])#.cuda()
 evc_nois = torch.from_numpy(data['eigvects_nois_avg'])#.cuda()
 evc_all = torch.from_numpy(data['eigvects_avg']).cuda()
 evc_sep = torch.from_numpy(block_diag(data['eigvects_nois_avg'], data['eigvects_clas_avg'])).cuda()
 evc_none = torch.eye(256).cuda()
 #%%
-from imageio import imread
-target = imread("block042_thread000_gen_gen041_001030.bmp")
-target_tsr = torch.from_numpy(target / 255.0).permute([2, 0, 1]).unsqueeze(0)
-target_tsr = target_tsr.float().cuda()
-#%%
+"""Main function to do inversion."""
 def BasinCMA(target_tsr, cmasteps=30, gradsteps=40, finalgrad=500, batch_size=4,
-             basis="all", CMApostAdam=False, RND=None):
+             basis="all", CMApostAdam=False, RND=None, savedir=savedir, imgnm=""):
     Record = {"L1cma": [],"dsimcma": [], "L1Adam": [], "dsimAdam": [], "L1refine":[], "dsimrefine":[], "classnorm":[], "noisenorm":[]}
     # choose the basis vector to use in Adam optimization
     basisvec = {"all": evc_all, "sep": evc_sep, "none": evc_none}[basis]
@@ -126,8 +154,8 @@ def BasinCMA(target_tsr, cmasteps=30, gradsteps=40, finalgrad=500, batch_size=4,
     codes_batch = torch.from_numpy(codes_post[idx[:4]]).float().cuda()
     fit_img = BGAN.generator(codes_batch, 0.7)
     fit_img = (fit_img + 1) / 2.0
-    CMAfitimg = ToPILImage()(make_grid(fit_img[:, :, :, :].cpu()))
-    CMAfitimg.save(join(savedir, "CMA_final%06d.jpg" % RND))
+    CMAfitimg = ToPILImage()(make_grid(torch.cat((fit_img, target_tsr)).cpu()))
+    CMAfitimg.save(join(savedir, "%s_CMA_final%06d.jpg" % (imgnm, RND)))
     CMAfitimg.show()
     # Linear Reparametrization using basisvec
     coef_batch = (codes_batch @ basisvec).detach().clone().requires_grad_(True)
@@ -154,86 +182,18 @@ def BasinCMA(target_tsr, cmasteps=30, gradsteps=40, finalgrad=500, batch_size=4,
                 step, dsims.mean().item(), L1dsim.mean().item(), latent_code.norm(dim=1).mean().item()))
     scores_final = dsims.detach().cpu().numpy()
     L1score_final = L1dsim.detach().cpu().numpy()
-    finalimg = ToPILImage()(make_grid(imgs[:, :, :, :].cpu()))
-    finalimg.save(join(savedir, "refinefinal%06d.jpg" % RND))
+    finalimg = ToPILImage()(make_grid(torch.cat((imgs, target_tsr)).cpu()))
+    finalimg.save(join(savedir, "%srefinefinal%06d.jpg" % (imgnm, RND)))
     finalimg.show()
 
     fig = visualize_optim(Record, titlestr="cmasteps %d gradsteps %d refinesteps %d Hbasis %s, CMApostAdam %d"%(cmasteps, gradsteps, finalgrad, basis, CMApostAdam))
-    fig.savefig(join(savedir, "traj_H%s%s_%d_dsim_%.3f_L1_%.3f.jpg" % (basis, "_postAdam" if CMApostAdam else "", RND, scores_final.min(), L1score_final.min())))
-    np.savez(join(savedir, "optim_data_%d.npz" % RND), codes=latent_code.cpu().detach().numpy(), Record=Record, dsims=scores_final, L1dsims=L1score_final)
+    fig.savefig(join(savedir, "%straj_H%s%s_%d_dsim_%.3f_L1_%.3f.jpg" % (imgnm, basis, "_postAdam" if CMApostAdam else "",
+                                                                        RND, scores_final.min(), L1score_final.min())))
+    np.savez(join(savedir, "%soptim_data_%d.npz" % (imgnm, RND)), codes=latent_code.cpu().detach().numpy(),
+             Record=Record, dsims=scores_final, L1dsims=L1score_final)
 
     return imgs.cpu().detach().numpy(), latent_code.cpu().detach().numpy(), scores_final, L1score_final, Record
 
-#%%
-imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=400, batch_size=4, basis="all")
-#%%
-imgs_np2, codes_np2, dsims2, L1dsims2, Record2 = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=400, batch_size=4, basis="all")
-#%%
-imgs_np3, codes_np3, dsims3, L1dsims3, Record3 = BasinCMA(target_tsr, cmasteps=10, gradsteps=20, finalgrad=400, batch_size=4, basis="all")
-#%%
-imgs_np4, codes_np4, dsims4, L1dsims4, Record4 = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=300, batch_size=4, basis="all")
-#%%
-
-#%%
-RND = np.random.randint(1E6)
-imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=5, gradsteps=5, finalgrad=400, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
-fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
-fig.savefig(join(savedir, "traj_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
-np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
-#%%
-RND = np.random.randint(1E6)
-imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
-fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
-fig.savefig(join(savedir, "traj_Hall_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
-np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
-#%%
-RND = np.random.randint(1E6)
-imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
-fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
-fig.savefig(join(savedir, "traj_Hall_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
-np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
-#%% This is pretty gfood
-imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=400, batch_size=4, basis="all", CMApostAdam=True)
-#%%
-imgs_np6, codes_np6, dsims6, L1dsims6, Record6 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none")
-#%%
-
-from time import time
-T0 = time()
-for trial in range(5):
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True)
-
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="sep", CMApostAdam=True)
-
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none", CMApostAdam=True)
-
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=False)
-
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="sep", CMApostAdam=False)
-
-    imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none", CMApostAdam=False)
-    print(time() - T0) # 5 hours
-
-#%%
-savedir = r"E:\OneDrive - Washington University in St. Louis\BigGAN_invert\BasinCMA\ImageNet"
-imgfolder = r"E:\Datasets\ImageNet\resize"
-
-for imgid in tqdm.trange(200, 205):
-    print("Processing image %d" %imgid)
-    imgnm = "val_crop_%08d"%imgid
-    img = imread(join(imgfolder, "val_crop_%08d.JPEG"%imgid))
-    target_tsr = torch.from_numpy(img / 255.0).permute([2, 0, 1]).unsqueeze(0)
-    target_tsr = target_tsr.float().cuda()
-
-    imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True)
-
-    imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=False)
-
-#%%
-# from argparse import ArgumentParser
-# parser = ArgumentParser()
-# parser.add_argument()
-import matplotlib.pylab as plt
 def visualize_optim(Record, titlestr="", savestr=""):
     fig, ax = plt.subplots()
     L1_cma_tr = np.array(Record["L1cma"])
@@ -263,23 +223,97 @@ def visualize_optim(Record, titlestr="", savestr=""):
     plt.show()
     return fig
 #%%
-L1_cma_tr = np.array(Record["L1cma"])
-dsim_cma_tr = np.array(Record["dsimcma"])
-L1_adam_tr = np.array(Record["L1Adam"])
-dsim_adam_tr = np.array(Record["dsimAdam"])
-dsim_tr = np.array(Record["dsimrefine"])
-L1_tr = np.array(Record["L1refine"])
-nos_norm = np.array(Record["noisenorm"])
-cls_norm = np.array(Record["classnorm"])
+from imageio import imread
+target = imread("block042_thread000_gen_gen041_001030.bmp")
+target_tsr = torch.from_numpy(target / 255.0).permute([2, 0, 1]).unsqueeze(0)
+target_tsr = target_tsr.float().cuda()
+#%%
+# savedir = r"E:\OneDrive - Washington University in St. Louis\BigGAN_invert\BasinCMA\ImageNet"
+# imgfolder = r"E:\Datasets\ImageNet\resize"
+# Append the name of parameters to the folder
+expdir = join(savedir, "CMA%dAdam%dFinal%d%s_%s"%(args.cmasteps, args.gradsteps, args.finalgradsteps,
+                                                  "_postAdam" if args.CMApostGrad else "", args.basis))
+os.makedirs(expdir, exist_ok=True)
+idmin, idmax = args.imgidx
+for imgid in tqdm.trange(idmin, idmax):
+    print("Processing image %d" %imgid)
+    imgnm = "val_crop_%08d"%imgid
+    img = imread(join(imgfolder, "val_crop_%08d.JPEG"%imgid))
+    target_tsr = torch.from_numpy(img / 255.0).permute([2, 0, 1]).unsqueeze(0)
+    target_tsr = target_tsr.float().cuda()
+    imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=args.cmasteps, gradsteps=args.gradsteps, finalgrad=args.finalgradsteps,
+                                                         batch_size=4, basis=args.basis, CMApostAdam=args.CMApostGrad,
+                                                         imgnm=imgnm, savedir=expdir)
+    # imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500,
+    #                                                      batch_size=4, basis="all", CMApostAdam=True)
 
-cma_steps, popsize = L1_cma_tr.shape
-xticks_arr = np.arange(-cma_steps, 0)[:,np.newaxis].repeat(popsize,1)
-plt.scatter(xticks_arr, L1_cma_tr, color="blue")
-plt.scatter(xticks_arr, dsim_cma_tr, color="green")
-plt.scatter(xticks_arr+0.2, L1_adam_tr, color="blue")
-plt.scatter(xticks_arr+0.2, dsim_adam_tr, color="green")
-plt.show()
-# plt.title("lr %.E beta1 %.3f beta2 %.3f wd_nos %.E wd_cls %.E gamma %.1f"%(lr,beta1,beta2,reg_w1,reg_w2,sched_gamma))
-# plt.savefig(join(savedir, "traj_Hallreg%06d_%.3f.jpg" % (RND, dsim.item())))
-# np.savez(join(savedir, "code_Hallreg%06d.jpg" % (RND,)), dsim=dsim.item(), scores_all=np.array(scores_all),
-#          nos_norm=np.array(nos_norm),cls_norm=np.array(cls_norm), code=latent_code.detach().cpu().numpy())
+#%%
+
+
+#%%
+# L1_cma_tr = np.array(Record["L1cma"])
+# dsim_cma_tr = np.array(Record["dsimcma"])
+# L1_adam_tr = np.array(Record["L1Adam"])
+# dsim_adam_tr = np.array(Record["dsimAdam"])
+# dsim_tr = np.array(Record["dsimrefine"])
+# L1_tr = np.array(Record["L1refine"])
+# nos_norm = np.array(Record["noisenorm"])
+# cls_norm = np.array(Record["classnorm"])
+#
+# cma_steps, popsize = L1_cma_tr.shape
+# xticks_arr = np.arange(-cma_steps, 0)[:, np.newaxis].repeat(popsize,1)
+# plt.scatter(xticks_arr, L1_cma_tr, color="blue")
+# plt.scatter(xticks_arr, dsim_cma_tr, color="green")
+# plt.scatter(xticks_arr+0.2, L1_adam_tr, color="blue")
+# plt.scatter(xticks_arr+0.2, dsim_adam_tr, color="green")
+# plt.show()
+
+
+#%%
+# imgs_np, codes_np, dsims, L1dsims, Record = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=400, batch_size=4, basis="all")
+# #%%
+# imgs_np2, codes_np2, dsims2, L1dsims2, Record2 = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=400, batch_size=4, basis="all")
+# #%%
+# imgs_np3, codes_np3, dsims3, L1dsims3, Record3 = BasinCMA(target_tsr, cmasteps=10, gradsteps=20, finalgrad=400, batch_size=4, basis="all")
+# #%%
+# imgs_np4, codes_np4, dsims4, L1dsims4, Record4 = BasinCMA(target_tsr, cmasteps=50, gradsteps=0, finalgrad=300, batch_size=4, basis="all")
+#
+# #%%
+# RND = np.random.randint(1E6)
+# imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=5, gradsteps=5, finalgrad=400, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
+# fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
+# fig.savefig(join(savedir, "traj_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
+# np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
+# #%%
+# RND = np.random.randint(1E6)
+# imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
+# fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
+# fig.savefig(join(savedir, "traj_Hall_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
+# np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
+# #%%
+# RND = np.random.randint(1E6)
+# imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True,  RND=RND)
+# fig = visualize_optim(Record5, titlestr="cmasteps5 gradsteps5 Hbasis all, CMApostAdam")
+# fig.savefig(join(savedir, "traj_Hall_%d_dsim_%.3f_L1_%.3f.jpg"%(RND, dsims5.min(), L1dsims5.min())))
+# np.savez(join(savedir, "optim_data_%d.npz"%RND), codes=codes_np5, Record=Record5, dsims=dsims5, L1dsims=L1dsims5)
+# #%% This is pretty gfood
+# imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=400, batch_size=4, basis="all", CMApostAdam=True)
+# #%%
+# imgs_np6, codes_np6, dsims6, L1dsims6, Record6 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none")
+# #%%
+#
+# from time import time
+# T0 = time()
+# for trial in range(5):
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=True)
+#
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="sep", CMApostAdam=True)
+#
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none", CMApostAdam=True)
+#
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="all", CMApostAdam=False)
+#
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="sep", CMApostAdam=False)
+#
+#     imgs_np5, codes_np5, dsims5, L1dsims5, Record5 = BasinCMA(target_tsr, cmasteps=10, gradsteps=10, finalgrad=500, batch_size=4, basis="none", CMApostAdam=False)
+#     print(time() - T0) # 5 hours
---
+++ cluster_scripts/BigGAN_invert_cma_cmp.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#PBS -N BigGAN_BasinCMA_invert
+#PBS -l nodes=1:ppn=1:gpus=1:K20,walltime=23:50:00,mem=10gb
+
+# Specify the default queue for the fastest nodes
+#PBS -m be
+#PBS -q dque
+#PBS -t 1-10
+
+# Prepare the virtual env for python
+# export PATH=/act/Anaconda3-2.3.0/bin:${PATH}
+# source activate conda_env
+export TORCH_HOME="/scratch/binxu/torch"
+#--cmasteps 10 --gradsteps 10 --finalgradsteps 500
+param_list='--imgidx 250 350 --CMApostGrad True --basis all
+--imgidx 250 350 --CMApostGrad True --basis none
+--imgidx 250 350 --CMApostGrad False --basis all
+--imgidx 250 350 --CMApostGrad False --basis none
+--imgidx 250 350 --cmasteps 50 --gradsteps 0 --basis all
+--imgidx 250 350 --cmasteps 50 --gradsteps 0 --basis none
+--imgidx 250 350 --cmasteps 1 --gradsteps 0 --finalgradsteps 600 --basis all
+--imgidx 250 350 --cmasteps 1 --gradsteps 0 --finalgradsteps 600 --basis none
+--imgidx 250 350 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis all
+--imgidx 250 350 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis none
+'
+export csr_lim="$(echo "$param_list" | head -n $PBS_ARRAYID | tail -1)"
+
+cd ~/Visual_Neuro_InSilico_Exp/
+python BigGAN_invert_cluster.py $csr_lim
+
