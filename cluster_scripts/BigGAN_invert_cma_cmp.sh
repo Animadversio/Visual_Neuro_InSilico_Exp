03324cda6b0c6c007634a3a6cd99ed278e286c21
Animadversio <binxu.wang@wustl.edu>
2020-08-07 00:36:53 -0500

    Add new cluster experiments

--- cluster_scripts/BigGAN_invert_cma_cmp.sh
+++ cluster_scripts/BigGAN_invert_cma_cmp.sh
@@ -6,7 +6,7 @@
 # Specify the default queue for the fastest nodes
 #PBS -m be
 #PBS -q dque
-#PBS -t 11-18
+#PBS -t 19-26
 
 # Prepare the virtual env for python
 # export PATH=/act/Anaconda3-2.3.0/bin:${PATH}
@@ -30,7 +30,15 @@ param_list='--imgidx 250 350 --CMApostGrad True --basis all
 --dataset BigGAN_rnd --imgidx 0 100 --cmasteps 50 --gradsteps 0 --basis all
 --dataset BigGAN_rnd --imgidx 0 100 --cmasteps 50 --gradsteps 0 --basis none
 --dataset BigGAN_rnd --imgidx 0 100 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis all
---dataset BigGAN_rnd --imgidx 0 100 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis none'
+--dataset BigGAN_rnd --imgidx 0 100 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis none
+--dataset BigGAN_rnd --imgidx 100 200 --CMApostGrad True --basis all
+--dataset BigGAN_rnd --imgidx 100 200 --CMApostGrad True --basis none
+--dataset BigGAN_rnd --imgidx 100 200 --CMApostGrad False --basis all
+--dataset BigGAN_rnd --imgidx 100 200 --CMApostGrad False --basis none
+--dataset BigGAN_rnd --imgidx 100 200 --cmasteps 50 --gradsteps 0 --basis all
+--dataset BigGAN_rnd --imgidx 100 200 --cmasteps 50 --gradsteps 0 --basis none
+--dataset BigGAN_rnd --imgidx 100 200 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis all
+--dataset BigGAN_rnd --imgidx 100 200 --cmasteps 1 --gradsteps 30 --finalgradsteps 600 --basis none'
 export csr_lim="$(echo "$param_list" | head -n $PBS_ARRAYID | tail -1)"
 
 cd ~/Visual_Neuro_InSilico_Exp/
