96935480e8dc9a8d62ed0b8e76157631dea0a8ad
Animadversio <binxu.wang@wustl.edu>
2020-08-17 01:14:31 -0500

    Add lots of analysis code

--- BigGAN_Evol_summary.py
+++ BigGAN_Evol_summary.py
@@ -138,6 +138,28 @@ for ui, unit_str in enumerate(unit_strs):
 align_tab = pd.DataFrame(align_col, columns=["unitstr", "net", "layer", "unit", "RND"]+list(methods_BigGAN))
 #%%
 align_tab.to_csv(join(summarydir, "optim_aligned_score_tab_BigGAN.csv"))
+#%%
+align_col_fc6 = []
+methods_all = exprec_tab.optimizer.unique()
+methods_fc6 = [method for method in methods_all if "_fc6" in method]
+fc6_msk = exprec_tab.optimizer.str.contains("_fc6")
+for ui, unit_str in enumerate(unit_strs):
+    unit_tup = unit_tups[ui]
+    mask = (exprec_tab.unitstr == unit_str) & fc6_msk  # experiments using fc6 GAN
+    RNDs = exprec_tab[mask].RND.unique()
+    for RND in RNDs:
+        entry = [unit_str, *unit_tup, RND, ]
+        for method in methods_fc6:
+            maskprm = mask & (exprec_tab.RND==RND) & (exprec_tab.optimizer==method)
+            try:
+                score = exprec_tab[maskprm].score.item()
+            except ValueError:
+                print("Imcomplete Entry %s (RND %d, unit %s)" % (method, RND, unit_str))
+                score = np.nan
+            entry.append(score)
+        align_col_fc6.append(tuple(entry))
+align_tab_fc6 = pd.DataFrame(align_col_fc6, columns=["unitstr", "net", "layer", "unit", "RND"]+list(methods_fc6))
+align_tab_fc6.to_csv(join(summarydir, "optim_aligned_score_tab_fc6.csv"))
 #%% Visualization
 import seaborn as sns
 sns.set()
@@ -200,21 +222,12 @@ ttest_rel(align_tab.HessCMA, align_tab.CholCMA, nan_policy='omit')
 ttest_rel(align_tab.HessCMA, align_tab.HessCMA_class, nan_policy='omit')
 ttest_rel(align_tab.HessCMA, align_tab.HessCMA_noA, nan_policy='omit')
 #%% Get sample images for each method! and montage them together.
-
-unit = ("fc6", 35)
-RND = ;
-method
-
-#%% Sample trajectory for each method!
 from imageio import imread
 from PIL import Image
-rectmp = exprec_tab.iloc[1000]
-imgid = 0
-img = imread(join(rootdir, rectmp.unitstr, "lastgen%s_%05d_score%.1f.jpg"%(rectmp.optimizer, rectmp.RND, rectmp.score)))
-nrow, ncol = img.shape[0]//258, img.shape[1]//258
-ri, ci = np.unravel_index(imgid, (nrow,ncol))
-img_crop = img[2+258*ri:258+258*ri,2+258*ci:258+258*ci,:]
-Image.fromarray(img_crop).show()
+from torchvision.utils import make_grid
+from build_montages import build_montages, color_framed_montages, make_grid_np
+
+exampdir = "E:\Cluster_Backup\BigGAN_Optim_Tune_new\summary\examplar"
 
 def read_lastgen(rec, imgid):
     img = imread(
@@ -223,15 +236,147 @@ def read_lastgen(rec, imgid):
     ri, ci = np.unravel_index(imgid, (nrow, ncol))
     img_crop = img[2 + 258 * ri:258 + 258 * ri, 2 + 258 * ci:258 + 258 * ci, :]
     Image.fromarray(img_crop).show()
+    return img_crop
 
 def read_bestimg_in_gen(rec, geni=100):
     try:
         img = imread(join(rootdir, rec.unitstr, "besteachgen%s_%05d.jpg"%(rec.optimizer, rec.RND, )))
     except FileNotFoundError:
-        img = imread(join(rootdir, rec.unitstr, "besteachgen%s_%05d.jpg" % (rec.optimizer, rec.RND, rec.score)))
-
-
+        img = imread(join(rootdir, rec.unitstr, "besteachgen%s_%05d_score%.1f.jpg" % (rec.optimizer, rec.RND,
+                                                                                     rec.score)))
     nrow, ncol = img.shape[0] // 258, img.shape[1] // 258  # 10, 10
-    ri, ci = np.unravel_index(geni, (nrow, ncol))
+    ri, ci = np.unravel_index(geni-1, (nrow, ncol))
     img_crop = img[2 + 258 * ri:258 + 258 * ri, 2 + 258 * ci:258 + 258 * ci, :]
     return img_crop
+
+#%%
+rectmp = exprec_tab.iloc[1000]
+imgid = 0
+img = imread(join(rootdir, rectmp.unitstr, "lastgen%s_%05d_score%.1f.jpg"%(rectmp.optimizer, rectmp.RND, rectmp.score)))
+nrow, ncol = img.shape[0]//258, img.shape[1]//258
+ri, ci = np.unravel_index(imgid, (nrow,ncol))
+img_crop = img[2+258*ri:258+258*ri,2+258*ci:258+258*ci,:]
+Image.fromarray(img_crop).show()
+
+#%% See the scores and images from the same initialization (Method comparison)
+# exprec_tab.optimizer.unique()
+rec = align_tab.iloc[200]
+recgroup = exprec_tab[(exprec_tab.unitstr == rec.unitstr) & (exprec_tab.RND == rec.RND)]
+print(recgroup[["unitstr","optimizer","score","RND"]])
+imgs_all = [read_bestimg_in_gen(reci) for _,reci in recgroup.iterrows()]
+mtg = make_grid_np(np.stack(imgs_all, 3))
+Image.fromarray(mtg).show()
+# mtg = build_montages(imgs_all, image_shape=(256, 256), montage_shape=(6,1))
+#%% See the scores and images from the Different initialization (Method x Init comparison)
+rec = align_tab.iloc[200]
+imgs_all = []
+trialRNDs = align_tab[align_tab.unitstr == rec.unitstr].RND.unique()  # trials for this units
+for trRND in trialRNDs:
+    recgroup = exprec_tab[(exprec_tab.unitstr == rec.unitstr) & (exprec_tab.RND == trRND)]
+    print(recgroup[["unitstr","optimizer","score","RND"]])
+    imgs_all.extend([read_bestimg_in_gen(reci) for _, reci in recgroup.iterrows()])
+mtg2 = make_grid_np(np.stack(imgs_all, 3), nrow=6)
+Image.fromarray(mtg2).show()
+
+#%% See the scores and images from the Different initialization (Method x Init x Space comparison)
+rec = align_tab.iloc[200]
+unitstr = rec.unitstr
+imgs_all = []
+scores_all = []
+methods = []
+trialRNDs = align_tab[align_tab.unitstr == unitstr].RND.unique()  # trials for this units
+for trRND in trialRNDs:
+    recgroup = exprec_tab[(exprec_tab.unitstr == unitstr) & (exprec_tab.RND == trRND)]
+    print(recgroup[["unitstr", "optimizer", "score", "RND"]])
+    imgs_all.extend([read_bestimg_in_gen(reci) for _, reci in recgroup.iterrows()])
+    scores_all.append(recgroup['score'].array)
+methods_BGAN = list(recgroup["optimizer"].array)
+mtg2 = make_grid_np(np.stack(imgs_all, 3), nrow=6)
+scores_all = np.array(scores_all)
+#%% Image.fromarray(mtg2).show()
+imgs_all_fc6 = []
+scores_all_fc6 = []
+trialRNDs_fc6 = align_tab_fc6[align_tab_fc6.unitstr == unitstr].RND.unique()  # trials for this units
+for trRND in trialRNDs_fc6:
+    recgroup = exprec_tab[(exprec_tab.unitstr == unitstr) & (exprec_tab.RND == trRND)]
+    print(recgroup[["unitstr", "optimizer", "score", "RND"]])
+    imgs_all_fc6.extend([read_bestimg_in_gen(reci) for _, reci in recgroup.iterrows()])
+    scores_all_fc6.append(recgroup['score'].array)
+methods_fc6 = list(recgroup["optimizer"].array)
+mtg3 = make_grid_np(np.stack(imgs_all_fc6, 3), nrow=3)
+scores_all_fc6 = np.array(scores_all_fc6)
+#%% Image.fromarray(mtg3).show()
+methods_all = methods_BGAN + methods_fc6
+scores_cat = np.hstack((scores_all, scores_all_fc6))
+mtg_all = Image.fromarray(np.hstack((mtg2, mtg3),))
+mtg_all.show()
+mtg_all.save(join(exampdir, "%s_repr_img.jpg" % unitstr))
+#%%
+# fig, ax = plt.subplots()
+# ax.matshow(scores_cat, cmap='viridis') # seismic
+# for (i, j), z in np.ndenumerate(scores_cat):
+#     ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center',
+#             bbox=dict(boxstyle='round', facecolor='lightgray', edgecolor='0.9'))
+# plt.show()
+plt.figure(figsize=[8, 6])
+ax = sns.heatmap(scores_cat, annot=True, fmt=".1f", xticklabels=methods_all)
+b, t = plt.ylim() # discover the values for bottom and top
+b += 0.5 # Add 0.5 to the bottom
+t -= 0.5 # Subtract 0.5 from the top
+plt.ylim(b, t)
+plt.xticks(rotation=30)
+plt.title("%s Optimizer, Space Comparison"%unitstr, )
+ax.figure.savefig(join(exampdir, "%s_scoremap.jpg" % unitstr))
+ax.figure.show()
+#%% Go through all the images
+
+# rec = align_tab.iloc[600]
+unit_bothspace = np.intersect1d(align_tab.unitstr.unique(), align_tab_fc6.unitstr.unique())
+for unitstr in unit_bothspace[11:]:
+    # unitstr = rec.unitstr
+    try:
+        imgs_all = []
+        scores_all = []
+        methods = []
+        trialRNDs = align_tab[align_tab.unitstr == unitstr].RND.unique()  # trials for this units
+        for trRND in trialRNDs:
+            recgroup = exprec_tab[(exprec_tab.unitstr == unitstr) & (exprec_tab.RND == trRND)]
+            print(recgroup[["unitstr", "optimizer", "score", "RND"]])
+            imgs_all.extend([read_bestimg_in_gen(reci) for _, reci in recgroup.iterrows()])
+            scores_all.append(recgroup['score'].array)
+        methods_BGAN = list(recgroup["optimizer"].array)
+        mtg2 = make_grid_np(np.stack(imgs_all, 3), nrow=6)
+        scores_all = np.array(scores_all)
+        #% Image.fromarray(mtg2).show()
+        imgs_all_fc6 = []
+        scores_all_fc6 = []
+        trialRNDs_fc6 = align_tab_fc6[align_tab_fc6.unitstr == unitstr].RND.unique()  # trials for this units
+        for trRND in trialRNDs_fc6:
+            recgroup = exprec_tab[(exprec_tab.unitstr == unitstr) & (exprec_tab.RND == trRND)]
+            print(recgroup[["unitstr", "optimizer", "score", "RND"]])
+            imgs_all_fc6.extend([read_bestimg_in_gen(reci) for _, reci in recgroup.iterrows()])
+            scores_all_fc6.append(recgroup['score'].array)
+        methods_fc6 = list(recgroup["optimizer"].array)
+        mtg3 = make_grid_np(np.stack(imgs_all_fc6, 3), nrow=3)
+        scores_all_fc6 = np.array(scores_all_fc6)
+        #% Image.fromarray(mtg3).show()
+        methods_all = methods_BGAN + methods_fc6
+        scores_cat = np.hstack((scores_all, scores_all_fc6))
+        mtg_all = Image.fromarray(np.hstack((mtg2, mtg3),))
+        mtg_all.show()
+        mtg_all.save(join(exampdir, "%s_repr_img.jpg" % unitstr))
+        #%%
+        plt.figure(figsize=[8, 6])
+        ax = sns.heatmap(scores_cat, annot=True, fmt=".1f", xticklabels=methods_all)
+        b, t = plt.ylim() # discover the values for bottom and top
+        plt.ylim(b + 0.5, t - 0.5)
+        plt.xticks(rotation=30)
+        plt.title("%s Optimizer, Space Comparison"%unitstr, )
+        plt.tight_layout()
+        ax.figure.savefig(join(exampdir, "%s_scoremap.jpg" % unitstr))
+        ax.figure.show()
+    except:
+        print("experiment non complete %s Check FC6 table"% unitstr)
+        print(align_tab_fc6[align_tab_fc6.unitstr==unitstr])
+
+#%% Sample trajectory for each method!
