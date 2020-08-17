a6e7a9892b16e221573f1e3ecd5ad88eeb33425d
Animadversio <binxu.wang@wustl.edu>
2020-08-17 01:14:43 -0500

    Add cluster task

--- cluster_scripts/BigGAN_Optimizer_cmp.sh
+++ cluster_scripts/BigGAN_Optimizer_cmp.sh
@@ -6,7 +6,7 @@
 # Specify the default queue for the fastest nodes
 #PBS -m be
 #PBS -q dque
-#PBS -t 41-56
+#PBS -t 57-72
 
 # Prepare the virtual env for python
 # export PATH=/act/Anaconda3-2.3.0/bin:${PATH}
@@ -67,7 +67,24 @@ param_list='--layer fc6 --chans 30 40 --optim HessCMA HessCMA_class HessCMA_noA 
 --layer conv2 --chans 50 60 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
 --layer conv3 --chans 50 60 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
 --layer conv4 --chans 50 60 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
---layer conv5 --chans 50 60 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5'
+--layer conv5 --chans 50 60 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv1 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv2 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv3 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv4 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv5 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer fc6 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer fc7 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer fc8 --chans 10 20 --G fc6 --optim HessCMA800 HessCMA500_1 CholCMA --steps 100 --reps 5
+--layer conv1 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer conv2 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer conv3 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer conv4 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer conv5 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer fc6 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer fc7 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+--layer fc8 --chans 10 20 --optim HessCMA HessCMA_class HessCMA_noA CholCMA CholCMA_prod CholCMA_class --steps 100 --reps 5
+'
 export csr_lim="$(echo "$param_list" | head -n $PBS_ARRAYID | tail -1)"
 
 cd ~/Visual_Neuro_InSilico_Exp/
