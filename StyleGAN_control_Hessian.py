fe77e55f7851cf42808104ff138e6b3e42ed7744
binxu <binxu.wang@wustl.edu>
2020-09-24 11:29:00 -0500

    Debug StyleGAN2 control and add StyleGAN control

--- StyleGAN_control_Hessian.py
+++ StyleGAN_control_Hessian.py
@@ -12,15 +12,15 @@ import lpips
 from GAN_hessian_compute import hessian_compute, get_full_hessian
 from torchvision.transforms import ToPILImage
 from torchvision.utils import make_grid
-from GAN_utils import loadBigGAN, loadStyleGAN2, BigGAN_wrapper, StyleGAN2_wrapper
+from GAN_utils import loadBigGAN, loadStyleGAN2, BigGAN_wrapper, StyleGAN2_wrapper, loadStyleGAN, StyleGAN_wrapper
 from hessian_analysis_tools import plot_spectra, compute_hess_corr
-from hessian_analysis_tools import scan_hess_npz, average_H, plot_consistentcy_mat, plot_consistency_hist, plot_consistency_example
 from lpips import LPIPS
 ImDist = LPIPS(net="squeeze")
-datadir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN2"
+datadir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN"
+os.makedirs(datadir, exist_ok=True)
 #%%
-SGAN = loadStyleGAN2('ffhq-512-avg-tpurun1.pt')
-SD = SGAN.state_dict()
+StyleGAN = loadStyleGAN()
+SD = StyleGAN.state_dict()
 #%%
 shuffled_SD = {}
 for name, Weight in SD.items():
@@ -28,12 +28,12 @@ for name, Weight in SD.items():
     W_shuf = Weight.view(-1)[idx].view(Weight.shape)
     shuffled_SD[name] = W_shuf
 #%%
-torch.save(shuffled_SD, join(datadir, "StyleGAN2_ffhq-512-avg-tpurun1_shuffle.pt"))
+torch.save(shuffled_SD, join(datadir, "StyleGAN_shuffle.pt"))
     # print(name, Weight.shape, Weight.mean().item(), Weight.std().item())
 #%%
-SGAN_sf = loadStyleGAN2('ffhq-512-avg-tpurun1.pt')
-SGAN_sf.load_state_dict(torch.load(join(datadir, "StyleGAN2_ffhq-512-avg-tpurun1_shuffle.pt")))
-G_sf = StyleGAN2_wrapper(SGAN_sf)
+StyleGAN_sf = loadStyleGAN()
+StyleGAN_sf.load_state_dict(torch.load(join(datadir, "StyleGAN_shuffle.pt")))
+G_sf = StyleGAN_wrapper(StyleGAN_sf)
 #%%
 img = G_sf.visualize(torch.randn(1,512).cuda()).cpu()
 ToPILImage()(img[0,:].cpu()).show()
@@ -46,33 +46,33 @@ def Hess_hook(module, fea_in, fea_out):
     L2dist_col.append(L2dist)
     return None
 
-triali = 0
-savedir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN2\ctrl_Hessians"
+savedir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN\ctrl_Hessians"
 os.makedirs(savedir, exist_ok=True)
-for triali in tqdm(range(42, 50)):
+for triali in tqdm(range(0, 100)):
     feat = torch.randn(1,512).cuda()
     eigvals, eigvects, H = hessian_compute(G_sf, feat, ImDist, hessian_method="BP", )
     np.savez(join(savedir, "eig_full_trial%d.npz"%(triali)), H=H, eva=eigvals, evc=eigvects,
                  feat=feat.cpu().detach().numpy())
-    feat.requires_grad_(True)
-    for blocki in [0, 3, 5, 8, 10, 12]:
-        L2dist_col = []
-        torch.cuda.empty_cache()
-        H1 = SGAN_sf.convs[blocki].register_forward_hook(Hess_hook)
-        img = SGAN_sf([feat], truncation=1)
-        H1.remove()
-        T0 = time()
-        H00 = get_full_hessian(L2dist_col[0], feat)
-        eva00, evc00 = np.linalg.eigh(H00)
-        print("Spent %.2f sec computing" % (time() - T0))
-        np.savez(join(savedir, "eig_genBlock%02d_trial%d.npz"%(blocki, triali)), H=H00, eva=eva00, evc=evc00,
-                 feat=feat.cpu().detach().numpy())
+    # feat.requires_grad_(True)
+    # for blocki in [0, 3, 5, 8, 10, 12]:
+    #     L2dist_col = []
+    #     torch.cuda.empty_cache()
+    #     H1 = SGAN_sf.convs[blocki].register_forward_hook(Hess_hook)
+    #     img = SGAN_sf([feat], truncation=1)
+    #     H1.remove()
+    #     T0 = time()
+    #     H00 = get_full_hessian(L2dist_col[0], feat)
+    #     eva00, evc00 = np.linalg.eigh(H00)
+    #     print("Spent %.2f sec computing" % (time() - T0))
+    #     np.savez(join(savedir, "eig_genBlock%02d_trial%d.npz"%(blocki, triali)), H=H00, eva=eva00, evc=evc00,
+    #              feat=feat.cpu().detach().numpy())
 #%%
 from hessian_analysis_tools import scan_hess_npz, average_H, plot_consistentcy_mat, plot_consistency_hist, plot_consistency_example
-savedir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN2\ctrl_Hessians"
-figdir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN2"
+savedir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN\ctrl_Hessians"
+figdir = r"E:\OneDrive - Washington University in St. Louis\HessNetArchit\StyleGAN"
+realfigdir = r"E:\OneDrive - Washington University in St. Louis\Hessian_summary\StyleGAN"
 
-modelnm = "StyleGAN2_Face512_shuffle"
+modelnm = "StyleGAN_shuffle"
 # Load the Hessian NPZ
 eva_ctrl, evc_ctrl, feat_ctrl, meta = scan_hess_npz(savedir, "eig_full_trial(\d*).npz", evakey='eva', evckey='evc', featkey="feat")
 # compute the Mean Hessian and save
@@ -88,24 +88,21 @@ fig11, fig22 = plot_consistency_hist(corr_mat_log_ctrl, corr_mat_lin_ctrl, figdi
                                     savelabel=modelnm)
 fig3 = plot_consistency_example(eva_ctrl, evc_ctrl, figdir=figdir, nsamp=5, titstr="%s"%modelnm, savelabel=modelnm)
 #%%
-realfigdir = r"E:\OneDrive - Washington University in St. Louis\Hessian_summary\StyleGAN2"
-with np.load(join(figdir, "spectra_col_%s.npz"%"StyleGAN2_Face512_shuffle")) as data:
+with np.load(join(figdir, "spectra_col_%s.npz"%modelnm)) as data:
     eva_ctrl = data["eigval_col"]
-with np.load(join(realfigdir, "spectra_col_%s.npz"%"ffhq-512-avg-tpurun1")) as data:
+with np.load(join(realfigdir, "spectra_col_BP.npz")) as data:
     eva_real = data["eigval_col"]
-fig0 = plot_spectra(eva_real, savename="StyleGAN2_Face512_shuffle_spectrum_cmp", figdir=figdir, abs=True,
-            titstr="StyleGAN2_Face512 cmp", label="real", fig=None)
-fig0 = plot_spectra(eva_ctrl, savename="StyleGAN2_Face512_shuffle_spectrum_cmp", figdir=figdir, abs=True,
-            titstr="StyleGAN2_Face512 cmp", label="shuffled", fig=fig0)
+fig0 = plot_spectra(eva_real, savename="StyleGAN_shuffle_spectrum_cmp", figdir=figdir, abs=True,
+            titstr="StyleGAN cmp", label="real", fig=None)
+fig0 = plot_spectra(eva_ctrl, savename="StyleGAN_shuffle_spectrum_cmp", figdir=figdir, abs=True,
+            titstr="StyleGAN cmp", label="shuffled", fig=fig0)
 #%%
-with np.load(join(realfigdir, "Hess_ffhq-512-avg-tpurun1_corr_mat.npz")) as data:
+with np.load(join(realfigdir, "Hess__corr_mat.npz")) as data:
     corr_mat_log, corr_mat_lin = data["corr_mat_log"], data["corr_mat_lin"]
-with np.load(join(figdir, "Hess_%s_corr_mat.npz"%"StyleGAN2_Face512_shuffle")) as data:
-    corr_mat_log_ctrl, corr_mat_lin_ctrl = data["corr_mat_log"], data["corr_mat_lin"]
 fig11, fig22 = plot_consistency_hist(corr_mat_log, corr_mat_lin, figdir=figdir, titstr="%s"%"real",
-                                    savelabel="StyleGAN2_Face512_shuffle_cmp")
+                                    savelabel="StyleGAN_shuffle_cmp")
 fig11, fig22 = plot_consistency_hist(corr_mat_log_ctrl, corr_mat_lin_ctrl, figdir=figdir, titstr="%s"%"shuffle",
-                                    savelabel="StyleGAN2_Face512_shuffle_cmp", figs=(fig11, fig22))
+                                    savelabel="StyleGAN_shuffle_cmp", figs=(fig11, fig22))
 #%%
-eva_col, evc_col, meta = scan_hess_npz(r"E:\Cluster_Backup\StyleGAN2\ffhq-512-avg-tpurun1")
-np.savez(join(realfigdir, "spectra_col_%s.npz"%"ffhq-512-avg-tpurun1"), eigval_col=eva_col)
+# eva_col, evc_col, meta = scan_hess_npz(r"E:\Cluster_Backup\StyleGAN2\ffhq-512-avg-tpurun1")
+# np.savez(join(realfigdir, "spectra_col_%s.npz"%"ffhq-512-avg-tpurun1"), eigval_col=eva_col)
