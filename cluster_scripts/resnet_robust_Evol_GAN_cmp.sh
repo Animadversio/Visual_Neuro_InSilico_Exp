ead59bd6f010bc2ad9ced29052d6d14051442d15
binxu.wang <binxu.wang@wustl.edu>
2021-09-06 11:42:53 -0500

    add

---
+++ cluster_scripts/resnet_robust_Evol_GAN_cmp.sh
@@ -0,0 +1,10 @@
+
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G BigGAN --net resnet50_linf_8 --layer .layer3.Bottleneck4 --optim HessCMA CholCMA --rep 3 # --RFresize True
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G fc6 --net resnet50_linf_8 --layer .layer3.Bottleneck4 --optim HessCMA500  --rep 3 # --RFresize True
+
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G BigGAN --net resnet50_linf_8 --layer .layer4.Bottleneck2 --optim HessCMA CholCMA --rep 3 # --RFresize True
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G fc6 --net resnet50_linf_8 --layer .layer4.Bottleneck2 --optim HessCMA500  --rep 3 # --RFresize True
+
+
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G BigGAN --net resnet50_linf_8 --layer .Linearfc --optim HessCMA CholCMA --rep 3 # --RFresize True
+python BigGAN_Evol_cmp_RIS_cluster.py --chans 0 50 --G fc6 --net resnet50_linf_8 --layer .Linearfc --optim HessCMA500  --rep 3 # --RFresize True
