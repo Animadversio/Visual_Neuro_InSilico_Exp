46848098ffe9636c2be8ca2962e0d0336ed35617
binxu.wang <binxu.wang@wustl.edu>
2021-09-06 23:32:15 -0500

    add

--- cluster_scripts/RIS_job_demo.sh
+++ cluster_scripts/RIS_job_demo.sh
@@ -10,6 +10,7 @@
 #BSUB -a 'docker(alpine)'
 #BSUM -u binxu.wang@wustl.edu
 #BSUM -N
+echo "$LSB_JOBINDEX"
 
 param_list='1
 2
@@ -22,10 +23,11 @@ param_list='1
 9
 '
 
-echo "$LSB_JOBINDEX"
-
 export unit_name="$(echo "$param_list" | head -n $LSB_JOBINDEX | tail -1)"
 # Append the extra command to the script.
 cd ~/Visual_Neuro_InSilico_Exp/
 echo "$unit_name"
+
+
+
 # python insilico_ResizeManifold_torch_efficient_script.py  $unit_name
