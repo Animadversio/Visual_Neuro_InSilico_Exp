9ac0384577dc222560ce1f1a5580206666c9d2ed
Animadversio <binxu.wang@wustl.edu>
2021-10-01 11:28:34 -0400

    movie time scale

--- CNN_movie_timescale_cmp.py
+++ CNN_movie_timescale_cmp.py
@@ -4,10 +4,13 @@ from os.path import join
 simclr_path = r"D:\DL_Projects\Vision\SimCLRv2-Pytorch"
 sys.path.append(simclr_path)
 from resnet import get_resnet, name_to_params
+from NN_timescale_lib import *
+#%%
 pth_path = join(simclr_path,"r50_1x_sk0_ema.pth")
 model, _ = get_resnet(*name_to_params(pth_path))
 model.load_state_dict(torch.load(pth_path)['resnet'])
-from NN_timescale_lib import *
+
+# ResNet self-supervised
 scorer = TorchScorer("resnet50")
 scorer.model = model.cuda()
 module_names, module_types, module_spec = get_module_names(scorer.model,[3,227,227],"cuda")
@@ -27,7 +30,7 @@ recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
         popsize=2000, run_frames=50000, seglen=1000, batch=120, video_id="goprorun",
         video_path=r"E:\Datasets\POVfootage\GoPro Hero 8 Running Footage with Head Mount.mp4",
         savedir=r"E:\insilico_exps\CNN_timescale\resnet_ssl_run", savenm="resnet_ssl_run")
-#%%
+#%% ResNet self-supervised
 model, _ = get_resnet(*name_to_params(pth_path))
 model.load_state_dict(torch.load(pth_path)['resnet'])
 scorer = TorchScorer("resnet50")
@@ -37,20 +40,20 @@ recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
         popsize=2000, run_frames=50000, seglen=1000, batch=120, video_id="goprobike",
         video_path=r"E:\Datasets\POVfootage\One Hour of Beautiful MTB POV Footage _ The Loam Ranger.mp4",
         savedir=r"E:\insilico_exps\CNN_timescale\resnet_ssl_bike", savenm="resnet_ssl_bike")
-#%%
+#%% ResNet robust
 from NN_timescale_lib import *
 targetnames = [ ".ReLUrelu",
-                   ".layer1.Bottleneck0",
-                   ".layer1.Bottleneck2",
-                   ".layer2.Bottleneck0",
-                   ".layer2.Bottleneck2",
-                   ".layer3.Bottleneck0",
-                   ".layer3.Bottleneck2",
-                   ".layer3.Bottleneck4",
-                   ".layer4.Bottleneck0",
-                   ".layer4.Bottleneck2",
-                   ".Linearfc"
-                   ]
+               ".layer1.Bottleneck0",
+               ".layer1.Bottleneck2",
+               ".layer2.Bottleneck0",
+               ".layer2.Bottleneck2",
+               ".layer3.Bottleneck0",
+               ".layer3.Bottleneck2",
+               ".layer3.Bottleneck4",
+               ".layer4.Bottleneck0",
+               ".layer4.Bottleneck2",
+               ".Linearfc"
+               ]
 scorer = TorchScorer("resnet50_linf_8")
 # scorer.model = model.cuda()
 recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
@@ -66,7 +69,7 @@ recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
         video_path=r"E:\Datasets\POVfootage\GoPro Hero 8 Running Footage with Head Mount.mp4",
         savedir=r"E:\insilico_exps\CNN_timescale\resnet_linf8_run", savenm="resnet_linf8_run")
 
-#%%
+#%% VGG16
 from NN_timescale_lib import *
 targetnames = [".features.ReLU1",
                ".features.ReLU3",
@@ -92,3 +95,74 @@ recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
         popsize=1000, run_frames=50000, seglen=1000, batch=60, video_id="goprorun",
         video_path=r"E:\Datasets\POVfootage\GoPro Hero 8 Running Footage with Head Mount.mp4",
         savedir=r"E:\insilico_exps\CNN_timescale\vgg16_run", savenm="vgg16_linf8_run")
+#%% CorNet-s
+from NN_timescale_lib import *
+scorer = TorchScorer("cornet_s")
+targetnames = [ ".V1.ReLUnonlin1",
+                ".V1.ReLUnonlin2",
+                ".V2.Conv2dconv_input",
+                ".CORblock_SV2",
+                ".V4.Conv2dconv_input",
+                ".CORblock_SV4",
+                ".IT.Conv2dconv_input",
+                ".CORblock_SIT",
+                ".decoder.Linearlinear",
+               ]
+recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
+        timescale_analysis_pipeline(scorer, targetnames,
+        popsize=1000, run_frames=50000, seglen=1000, batch=120, video_id="goprorun",
+        video_path=r"E:\Datasets\POVfootage\GoPro Hero 8 Running Footage with Head Mount.mp4",
+        savedir=r"E:\insilico_exps\CNN_timescale\cornet_run", savenm="cornet_run")
+
+scorer = TorchScorer("cornet_s")
+recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
+        timescale_analysis_pipeline(scorer, targetnames,
+        popsize=1000, run_frames=50000, seglen=1000, batch=120, video_id="goprobike",
+        video_path=r"E:\Datasets\POVfootage\One Hour of Beautiful MTB POV Footage _ The Loam Ranger.mp4",
+        savedir=r"E:\insilico_exps\CNN_timescale\cornet_bike", savenm="cornet_bike")
+
+#%% ResNet Trained with DINO
+targetnames = [ ".ReLUrelu",
+               ".layer1.Bottleneck0",
+               ".layer1.Bottleneck2",
+               ".layer2.Bottleneck0",
+               ".layer2.Bottleneck2",
+               ".layer3.Bottleneck0",
+               ".layer3.Bottleneck2",
+               ".layer3.Bottleneck4",
+               ".layer4.Bottleneck0",
+               ".layer4.Bottleneck2",
+               ".Identityfc"
+               ]
+
+scorer = TorchScorer("resnet50")
+scorer.model = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50').cuda()
+recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
+        timescale_analysis_pipeline(scorer, targetnames,
+        popsize=1000, run_frames=50000, seglen=1000, batch=120, video_id="goprorun",
+        video_path=r"E:\Datasets\POVfootage\GoPro Hero 8 Running Footage with Head Mount.mp4",
+        savedir=r"E:\insilico_exps\CNN_timescale\resnet50_dino_run", savenm="resnet50_dino_run")
+
+scorer = TorchScorer("resnet50")
+scorer.model = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50').cuda()
+recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
+        timescale_analysis_pipeline(scorer, targetnames,
+        popsize=1000, run_frames=50000, seglen=1000, batch=120, video_id="goprobike",
+        video_path=r"E:\Datasets\POVfootage\One Hour of Beautiful MTB POV Footage _ The Loam Ranger.mp4",
+        savedir=r"E:\insilico_exps\CNN_timescale\resnet50_dino_bike", savenm="resnet50_dino_bike")
+
+#%% Visual Transformer.
+vitb8 = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
+targetnames = [".blocks.Block0",
+               ".blocks.Block1",
+               ".blocks.Block7",
+               ".blocks.Block11"]
+scorer = TorchScorer("resnet50")
+del scorer.model
+scorer.model = vitb8.cuda()
+#%%
+recordings_all, acf_arr_dict, df, unit_mask_dict, figh, figh2 = \
+        timescale_analysis_pipeline(scorer, targetnames,
+        popsize=1000, run_frames=50000, seglen=1000, batch=40, video_id="goprobike",
+        video_path=r"E:\Datasets\POVfootage\One Hour of Beautiful MTB POV Footage _ The Loam Ranger.mp4",
+        savedir=r"E:\insilico_exps\CNN_timescale\vitB8_dino_bike", savenm="vitB8_dino_bike")
